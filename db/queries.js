441d099799e4b99694012d4ab6944965328564a0
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-09-19 12:52:06 -0400

    added messages to home page

--- db/queries.js
+++ db/queries.js
@@ -9,4 +9,23 @@ async function createUser(firstName, lastName, username, hashedPassword) {
   );
 }
 
-module.exports = { createUser };
+// Get all messages
+async function getMessages() {
+  const { rows } = await pool.query(
+    `SELECT messages.title, messages.text, messages.timestamp, users.username
+     FROM messages
+     JOIN users ON messages.user_id = users.id
+     ORDER BY messages.timestamp DESC`
+  );
+  return rows;
+}
+
+// Add Message Handler
+async function addMessage(title, message, authorId, date) {
+  await pool.query(
+    "INSERT INTO messages (title, text, user_id, timestamp) VALUES ($1, $2, $3, $4)",
+    [title, message, authorId, date]
+  );
+}
+
+module.exports = { createUser, getMessages, addMessage };
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 var express = require("express");
 var router = express.Router();
+const db = require("../db/queries");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", {
-    title: "Members Only",
-    user: req.user,
-    messages: req.session.messages || [],
-  });
+// GET home page.
+router.get("/", async function (req, res, next) {
+  try {
+    const messages = await db.getMessages();
+    const loginFeedback = req.session.messages || []; // Passport message
+    req.session.messages = []; // Clear passport messages
+    console.log("loginFeedback", req.session.messages);
+    res.render("index", {
+      title: "Members Only",
+      user: req.user,
+      messages: messages || [],
+      loginFeedback,
+    });
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    next(error);
+  }
 });
 
-/* GET membership page */
+// GET membership page
 router.get("/secret", function (req, res, next) {
   if (req.isAuthenticated()) {
     res.render("secret", { user: req.user });
@@ -19,7 +30,7 @@ router.get("/secret", function (req, res, next) {
   }
 });
 
-/* GET log out */
+// GET log out
 router.get("/logOut", function (req, res, next) {
   req.logout(function (err) {
     if (err) {
@@ -27,6 +38,22 @@ router.get("/logOut", function (req, res, next) {
     }
     res.redirect("/");
   });
+});
+
+// Add message, then render index
+router.post("/addMessage", async function (req, res, next) {
+  try {
+    await db.addMessage(
+      req.body.title,
+      req.body.message,
+      req.user.id,
+      new Date()
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error adding message:", error);
+    next(error);
+  }
 });
 
 module.exports = router;
