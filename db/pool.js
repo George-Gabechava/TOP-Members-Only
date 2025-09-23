5e81ed8bf535de321072aff89629f8d361b71d37
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-09-23 16:29:02 -0400

    added CSS, added Sign Up Error display

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// For deployment on Railway
+// Once deployed on Railway
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
